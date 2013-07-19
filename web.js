374106765df72029d3cc40496c3b7b87749c18a8
Rafael MS <rjm84s@gmail.com>
2013-07-19 02:42:07 +0000

    update

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs');
-var buffer - new Buffer(256);
+var buffer = new Buffer(256);
   response.send(fs.readFileSync('index.html').toString());
 });
 
